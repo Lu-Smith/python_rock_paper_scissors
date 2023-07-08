32e28cf414925eeceea5ef09f0367c00bc2c9dd6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-08 15:54:39 +0100

    Updated logic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,6 +4,12 @@ user_wins = 0
 computer_wins = 0
 
 while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit").lower()
+    user_input = input("Type Rock/Paper/Scissors or Q to quit ").lower()
     if user_input == "q":
-        quit()
+        break
+
+    if user_input not in ["rock", "paper", "scissors"]:
+        continue
+
+print("Goodbye")
+
