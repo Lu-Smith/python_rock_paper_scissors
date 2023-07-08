0ff62b1dfbf015ed7174bdc7b470a57bf8d08b08
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-08 16:27:04 +0100

    Updated logic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -13,11 +13,26 @@ while True:
     if user_input not in options:
         continue
 
-    random_number = random.randindt(0, 2)
+    random_number = random.randint(0, 2)
     # rock: 0, paper: 1, scissors: 2
     computer_guess = options[random_number]
     print("Computer picked, " + computer_guess + ".")
 
+    if user_input == "rock" and computer_guess == "scissors":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "scissors" and computer_guess == "paper":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "paper" and computer_guess == "rock":
+        print("You won!")
+        user_wins += 1
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("The computer won", computer_wins, "times.")
+print("You won", user_wins, "times.")
 
 print("Goodbye")
 
